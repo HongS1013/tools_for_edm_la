f5c38abeac58bc1579e24a088b7901d8bd895d9c
HongS <41907842+HongS1013@users.noreply.github.com>
2023-10-15 17:40:41 -0500

    Update viewer.js

--- viewer/web/viewer.js
+++ viewer/web/viewer.js
@@ -4119,7 +4119,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: 'siemens-2013-learning-analytics-the-emergence-of-a-discipline.pdf',
+    value: 'tools-for-educational-data-mining-a-review.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
